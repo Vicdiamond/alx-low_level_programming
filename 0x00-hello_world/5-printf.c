ce52cf30a1c64af50b1663affc14db3b317e33a8
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-13 10:04:32 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main entry of the program
+ *
+ * Description: This runs the task given
+ * Return: It return no integer
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
