59ffa8bce3af79230cf379f20b53ed208ad040ec
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-13 14:58:17 -0700

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Description: It does that the task asked us to do
+ * Return: It does not return any integer
+ */
+int main(void)
+{
+	write(1, "amd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
