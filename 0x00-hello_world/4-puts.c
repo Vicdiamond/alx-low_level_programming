c4652c64435cad950fe1220b3133376ac0045c60
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-13 09:50:12 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry function
+ *
+ * Description: This runs the tasks
+ * Return: It does not return anything
+ *
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
