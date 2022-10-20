f1bd1ad398a414384df30956dab64f4416f3c20d
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-20 14:05:40 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
  */
 void print_most_numbers(void)
 {
-        int number;
-        for (number = '0'; number <= '9'; number++)
-        {
-                if (number == '2' || number == '4')
-                        ;
-                else
-                        _putchar(number);
-        }
-        _putchar('\n');
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
 }
