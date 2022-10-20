17fcf3c4ece3502764cd12c8ed98ba7ff4eec40c
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-20 06:19:49 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 - 9
+ *
+ * Description: It performs the given tasks
+ *
+ *  Return: Always return 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c >= 9; c++)
+	{
+		_putchar(c);
+		_putchar("\n");
+	}
+}
