33677f338d1b16e66872cf94482fde6c7e0541ba
Modestus Victor <Modestusvictor3@gmail.com>
2022-10-20 06:49:54 -0700

    task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,9 +12,10 @@ void print_numbers(void)
 {
 	int c;
 
-	for (c >= '0'; c <= '9'; c++)
+	for (c = 0; < 9; c++)
 	{
-		_putchar(c);
-		_putchar("\n");
+		_putchar(c + '0');
+		c++
 	}
+	_putchar("\n");
 }
